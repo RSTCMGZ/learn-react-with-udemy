0a31da0f015131f07e4bb5d10579e91e42dad701
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-01-11 22:52:48 +0300

    css-write

--- Task/jira-application/src/components/TaskShow.js
+++ Task/jira-application/src/components/TaskShow.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function TaskShow({ task }) {
-    console.log(task);
     return (
-        <div>
+        <div className='task-show'>
+            <h3 className='task-title'>Göreviniz</h3>
+            <p>{task.title}</p>
+            <p className='task-title'>Yapılacaklar</p>
+            <p>{task.taskDesc}</p>
+            <div>
+                <button className='task-delete'>Sil</button>
+                <button className='task-edit'>Güncelle</button>
+            </div>
         </div>
     )
 }

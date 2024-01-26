d639cbd19b05de3a72780e334113f5386075b1ab
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-01-27 01:17:08 +0300

    reducer-learning

--- courses/mycourses/src/Courses.js
+++ courses/mycourses/src/Courses.js
@@ -1,22 +1,71 @@
-import Course from "./Course"
-
+import { useState } from 'react'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
 function Courses({ courses, removeCourse }) {
+    const [index, setIndex] = useState(0)
+    const { content, title, price } = courses[index]
+
+    const checkIndex = (index) => {
+        if (index < 0) {
+            return courses.length - 1
+        }
+        if (index > courses.length - 1) {
+            return 0
+        }
+        return index;
+    }
+
+    const prevCourse = () => {
+        setIndex((index) => {
+            let newIndex = index - 1
+            return checkIndex(newIndex);
+        })
+    }
+    const nextCourse = () => {
+        setIndex((index) => {
+            let newIndex = index + 1
+            return checkIndex(newIndex);
+        })
+    }
+
+    const getRandomCourse = () => {
+
+        let randomNumber = Math.floor(Math.random() * courses.length)
+        if (randomNumber === index) {
+            randomNumber = index + 1
+        }
+        setIndex(checkIndex(randomNumber))
+    }
+
     return (
         <div className="courseMainDiv">
-            <div>
+            <div className='courseTitleBtn'>
                 <h2>Kurslarım</h2>
+                <button onClick={getRandomCourse} className='cardDeleteBtn'>Rastgele Kurs Ata!</button>
 
             </div>
             <div className="cardDiv">
-                {
-                    courses.map((course) => {
-                        return (
-                            <Course key={course.id} {...course} removeOneCourse={removeCourse} />
-                            //course={course}
-                        )
-                    })
-                }
+                <button onClick={prevCourse} className="prevNextBtn">
+                    <FaChevronLeft />
+                </button>
+                <div className="card">
+                    <div className="cardTitlePrice">
+                        <h2 className="cardTitle">{title}</h2>
+                        <h4 className="cardPrice">{price}₺</h4>
+                    </div>
+                    <p>{content}</p>
+                </div>
+                <button onClick={nextCourse} className="prevNextBtn" >
+                    <FaChevronRight />
+                </button>
+                {/* {
+                     courses.map((course) => {
+                    //     return (
+                    //         <Course key={course.id} {...course} removeOneCourse={removeCourse} />
+                    //         //course={course}
+                    //     )
+                     })
+                } */}
             </div>
         </div>
     )

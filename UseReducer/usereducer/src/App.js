1bcdef04f71f9fb28111304fd35a197a27d03ca7
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-01-28 15:26:19 +0300

    learnUseRef

--- UseReducer/usereducer/src/App.js
+++ UseReducer/usereducer/src/App.js
@@ -1,39 +1,68 @@
 import './App.css';
-import React, { useEffect, useReducer } from 'react'
-import Calculate from './Calculate';
+// import React, { useEffect, useReducer } from 'react'
+// import Calculate from './Calculate';
+// export const NumberContext = React.createContext()
+import { useState, useEffect, useRef } from 'react'
 
-export const NumberContext = React.createContext()
 
 
+// const initialValue = 0
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case 'increment':
+//       return state + 1;
+//     case 'decrement':
+//       return state - 1;
+//     case 'reset':
+//       return initialValue;
+//     default:
+//       return state
+//   }
+// }
 
-const initialValue = 0
-const reducer = (state, action) => {
-  switch (action) {
-    case 'increment':
-      return state + 1;
-    case 'decrement':
-      return state - 1;
-    case 'reset':
-      return initialValue;
-    default:
-      return state
-  }
-}
 
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialValue)
+  // const [count, dispatch] = useReducer(reducer, initialValue)
+
+  // useEffect(() => {
+
+  // }, [count])
+  // return (
+  //   <div className="App">
+  //     <NumberContext.Provider value={{ count: count, dispatch: dispatch }}>
+  //       <Calculate />
+  //     </NumberContext.Provider>
+  //   </div>
+  // );
+  const [name, setName] = useState('')
+  // const [renderCount, setRenderCount] = useState(0)
+  const renderCount = useRef(0)
+  // { current: 0 }
+  const inputRef = useRef()
 
   useEffect(() => {
-
-  }, [count])
+    // setRenderCount((prev) => prev + 1)
+    renderCount.current = renderCount.current + 1
+  })
+  const focusInput = () => {
+    inputRef.current.focus()
+    inputRef.current.value = "Reso"
+  }
   return (
-    <div className="App">
-      <NumberContext.Provider value={{ count: count, dispatch: dispatch }}>
-        <Calculate />
-      </NumberContext.Provider>
+
+    <div className='App'>
+      <input
+        ref={inputRef}
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <div>Benim adım {name}</div>
+      <div>{renderCount.current} defa render oldu</div>
+      <button onClick={focusInput}>Focus</button>
     </div>
-  );
+  )
 }
 
 export default App;

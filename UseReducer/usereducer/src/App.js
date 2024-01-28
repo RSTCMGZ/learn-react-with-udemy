e6f0979d3f3a717a4298e7313c28de63f9411b29
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-01-28 22:44:48 +0300

    learnUseTransition

--- UseReducer/usereducer/src/App.js
+++ UseReducer/usereducer/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+// import Transition from './Transition';
 // import React, { useEffect, useReducer } from 'react'
 // import Calculate from './Calculate';
 // export const NumberContext = React.createContext()
-import { useState, useEffect, useRef } from 'react'
+// import { useState, useEffect, useRef } from 'react'
 
 
 
@@ -22,45 +23,59 @@ import { useState, useEffect, useRef } from 'react'
 
 
 
+// function App() {
+//   // const [count, dispatch] = useReducer(reducer, initialValue)
+
+//   // useEffect(() => {
+
+//   // }, [count])
+//   // return (
+//   //   <div className="App">
+//   //     <NumberContext.Provider value={{ count: count, dispatch: dispatch }}>
+//   //       <Calculate />
+//   //     </NumberContext.Provider>
+//   //   </div>
+//   // );
+//   // const [name, setName] = useState('')
+//   // // const [renderCount, setRenderCount] = useState(0)
+//   // const renderCount = useRef(0)
+//   // // { current: 0 }
+//   // const inputRef = useRef()
+
+//   // useEffect(() => {
+//   //   // setRenderCount((prev) => prev + 1)
+//   //   renderCount.current = renderCount.current + 1
+//   // })
+//   // const focusInput = () => {
+//   //   inputRef.current.focus()
+//   //   inputRef.current.value = "Reso"
+//   // }
+//   return (
+
+//     <div className='App'>
+//       {/* <input
+//         ref={inputRef}
+//         type='text'
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <div>Benim adım {name}</div>
+//       <div>{renderCount.current} defa render oldu</div>
+//       <button onClick={focusInput}>Focus</button> */}
+//       {/* <Transition /> */}
+//       {/* CUSTOM HOOK */}
+
+//     </div>
+//   )
+// }
+
+import React from 'react'
+import TitleOne from './TitleOne';
+
 function App() {
-  // const [count, dispatch] = useReducer(reducer, initialValue)
-
-  // useEffect(() => {
-
-  // }, [count])
-  // return (
-  //   <div className="App">
-  //     <NumberContext.Provider value={{ count: count, dispatch: dispatch }}>
-  //       <Calculate />
-  //     </NumberContext.Provider>
-  //   </div>
-  // );
-  const [name, setName] = useState('')
-  // const [renderCount, setRenderCount] = useState(0)
-  const renderCount = useRef(0)
-  // { current: 0 }
-  const inputRef = useRef()
-
-  useEffect(() => {
-    // setRenderCount((prev) => prev + 1)
-    renderCount.current = renderCount.current + 1
-  })
-  const focusInput = () => {
-    inputRef.current.focus()
-    inputRef.current.value = "Reso"
-  }
   return (
-
-    <div className='App'>
-      <input
-        ref={inputRef}
-        type='text'
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <div>Benim adım {name}</div>
-      <div>{renderCount.current} defa render oldu</div>
-      <button onClick={focusInput}>Focus</button>
+    <div>
+      <TitleOne />
     </div>
   )
 }
---
+++ UseReducer/usereducer/src/Transition.js
@@ -0,0 +1,33 @@
+import React, { useState, useTransition } from 'react'
+
+function Transition() {
+    const [input, setInput] = useState('')
+    const [myList, setMyList] = useState([])
+    const [isPending, startTransition] = useTransition()
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+
+        startTransition(() => {
+            const myArray = []
+            for (let i = 0; i < 2000; i++) {
+                myArray.push(e.target.value)
+            }
+            setMyList(myArray)
+        })
+    }
+    return (
+        <div>
+            <input
+                type='text'
+                value={input}
+                onChange={handleChange} />
+            {isPending ? 'Yükleniyor...' :
+                myList.map((item, index) => {
+                    return <div key={index}>{item}</div>
+                })}
+        </div >
+    )
+}
+
+export default Transition

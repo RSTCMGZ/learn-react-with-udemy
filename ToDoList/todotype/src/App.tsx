3bce844cf15717d5835fe1e6f998f1cc7498e7f8
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-07 01:42:37 +0300

    delete-add-function

--- ToDoList/todotype/src/App.tsx
+++ ToDoList/todotype/src/App.tsx
@@ -1,10 +1,49 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 import './App.css';
+import { todoType } from "./apptypes"
+import TodoItem from './TodoItem';
+
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>('')
+  const [workDay, setWorkDay] = useState<number>(0)
+  const [todoList, setTodoList] = useState<todoType[]>([])
+
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value)
+    } else {
+      setWorkDay(Number(event.target.value))
+    }
+  }
+  const addNewTask = () => {
+    const newTask = { taskName: task, workDay: workDay }
+    setTodoList([...todoList, newTask])
+    setTask('')
+    setWorkDay(0)
+  }
+  const deleteTask = (nameToDelete: string): void => {
+
+    setTodoList(todoList.filter((task) => {
+      return task.taskName !== nameToDelete
+    }))
+
+  }
+
+
   return (
     <div className="App">
-
+      <div>
+        <input type='text' onChange={handleChange} name='task' value={task} placeholder='Taskınızı giriniz...' />
+        <input type='number' onChange={handleChange} name='workDay' value={workDay} placeholder='Kaç günde tamamlamalısınız' />
+        <button onClick={addNewTask}>Yeni Task Ekle</button>
+      </div>
+      <div>
+        {todoList.map((task: todoType, index: number) => {
+          return <TodoItem key={index} task={task} deleteTask={deleteTask} />
+        })}
+      </div>
     </div>
   );
 }
---
+++ ToDoList/todotype/src/TodoItem.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { todoType } from "./apptypes"
+
+
+type PropsType = {
+    task: todoType,
+    deleteTask(nameToDelete: String): void
+}
+
+
+function TodoItem({ task, deleteTask }: PropsType) {
+    return (
+        <div>
+
+            <div>
+                <p>
+                    {task.taskName}
+                </p>
+                <p>
+                    {task.workDay}
+                </p>
+                <button onClick={() => deleteTask(task.taskName)}>Sil</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default TodoItem

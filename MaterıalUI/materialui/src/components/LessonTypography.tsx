01af8f91f7f107e242caf34688e3031928d12922
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-08 00:01:24 +0300

    learn-MaterıalUI-Typography

---
+++ MaterıalUI/materialui/src/components/LessonTypography.tsx
@@ -0,0 +1,21 @@
+import Typography from '@mui/material/Typography';
+
+const LessonTypography = () => {
+    return (
+        <div>
+            <Typography variant='body1'>Body1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque asperiores est dignissimos cupiditate, possimus, explicabo deserunt maxime quia adipisci rerum tempora libero id rem, quam labore qui non fugiat aliquid?</Typography>
+            <Typography variant='body2'>Body2 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque asperiores est dignissimos cupiditate, possimus, explicabo deserunt maxime quia adipisci rerum tempora libero id rem, quam labore qui non fugiat aliquid?</Typography>
+
+
+
+            <Typography variant='h1'>H1</Typography>
+            <Typography variant='h2'>H2</Typography>
+            <Typography variant='h3'>H3</Typography>
+            <Typography variant='h4'>H4</Typography>
+            <Typography variant='h5'>H5</Typography>
+            <Typography variant='h6'>H6</Typography>
+        </div>
+    )
+}
+
+export default LessonTypography

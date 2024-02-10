538aa2176a14bb7672853e6107295c3491d98c2c
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-10 23:49:26 +0300

    TextField-finish

--- MaterıalUI/materialui/src/components/LessonTextField.tsx
+++ MaterıalUI/materialui/src/components/LessonTextField.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import { Stack, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { InputAdornment, Stack, TextField } from '@mui/material'
 
 
 function LessonTextField() {
+
+    const [value, setValue] = useState('')
+
     return (
         <Stack spacing={4}>
             <Stack direction={'row'} spacing={2}>
@@ -10,6 +13,36 @@ function LessonTextField() {
                 <TextField label='Ad' variant='outlined' />
                 <TextField label='Ad' variant='filled' />
                 <TextField label='Ad' variant='standard' />
+                <TextField label='Ad' variant='outlined' size='small' color='error' />
+
+                <TextField label='Ad' variant='outlined'
+                    helperText='Lütfen isminizi giriniz' />
+
+                <TextField type='password' label='Ad' variant='outlined' helperText='Lütfen isminizi giriniz' />
+
+                <TextField type='password' label='Ad' variant='outlined' disabled helperText='Lütfen isminizi giriniz' />
+
+                <TextField type='password' label='Ad' variant='outlined' inputProps={{ readOnly: true }} helperText='Lütfen isminizi giriniz' />
+
+                <TextField label='Toplam' variant='outlined' inputProps={{
+                    startAdornment: (
+                        <InputAdornment position='start'>TL </InputAdornment>
+                    )
+                }} />
+
+                <TextField inputProps={{
+                    endAdornment: (
+                        <InputAdornment position='end'>TL </InputAdornment>
+                    )
+                }} />
+
+            </Stack>
+            <Stack direction={'row'}>
+                <TextField
+                    onChange={(e) => setValue(e.target.value)}
+                    value={value}
+                    type='password' label='Şifre' variant='outlined' helperText={!value ? 'Lütfen şifrenizi giriniz' : 'Şifrenizi kimseyle paylaşmayınız'} />
+
             </Stack>
         </Stack>
     )

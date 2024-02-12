f61fe3a5e47b5fd8dc32b67000599033ea7b4ca0
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-13 00:34:33 +0300

    Accordion-finish

--- MaterıalUI/materialui/src/components/LessonAccordion.tsx
+++ MaterıalUI/materialui/src/components/LessonAccordion.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import { Accordion, AccordionSummary, AccordionDetails, Typography, } from '@mui/material'
+import React, { useState } from 'react'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
 
 const LessonAccordion = () => {
+    const [expanded, setExpanded] = useState<string | false>(false)
+
+    const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+        setExpanded(isExpanded ? panel : false)
+    }
     return (
-        <div>LessonAccordion</div>
+        <>
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                >
+                    <Typography>ilk bölüm</Typography>
+                </AccordionSummary>
+                <AccordionDetails>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut nulla saepe necessitatibus ex, aperiam qui consequuntur quis numquam adipisci ullam, itaque voluptates, natus facere neque dolorem laborum a deleniti!</AccordionDetails>
+            </Accordion>
+            <Accordion
+                expanded={expanded === 'panel2'} onChange={handleChange('panel2')}
+            >
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                >
+                    <Typography>ikinci bölüm</Typography>
+                </AccordionSummary>
+                <AccordionDetails>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut nulla saepe necessitatibus ex, aperiam qui consequuntur quis numquam adipisci ullam, itaque voluptates, natus facere neque dolorem laborum a deleniti!</AccordionDetails>
+            </Accordion>
+            <Accordion
+                expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                >
+                    <Typography>ücüncü bölüm</Typography>
+                </AccordionSummary>
+                <AccordionDetails>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut nulla saepe necessitatibus ex, aperiam qui consequuntur quis numquam adipisci ullam, itaque voluptates, natus facere neque dolorem laborum a deleniti!</AccordionDetails>
+            </Accordion>
+        </>
+
     )
 }
 

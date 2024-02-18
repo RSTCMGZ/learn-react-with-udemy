2341563a997662272470da040e1385df436aaf15
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-18 22:45:00 +0300

    start-card-project-with-materialui

---
+++ card/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+function App() {
+  return (
+    <div>
+      <AppBar>
+        <Toolbar sx={{ fontSize: 30, }}>
+          Kart Projesi
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default App;

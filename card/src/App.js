bf0ab6d958125ac1102f19886e2dae5c1290a6dd
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-18 23:34:25 +0300

    finish-project

--- card/src/App.js
+++ card/src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Course from './Course';
+import Angular from "../src/images/angular.jpg"
+import Bootstrap from "../src/images/bootstrap5.png"
+import Ccsharp from "../src/images/ccsharp.png"
+import Komple from "../src/images/kompleweb.jpg"
 
 function App() {
   return (
@@ -10,6 +17,40 @@ function App() {
           Kart Projesi
         </Toolbar>
       </AppBar>
+      <Container maxWidth="lg">
+        <Grid container spacing={2} sx={{ marginTop: 10 }}>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course
+              image={Angular}
+              title='Angular'
+              description='   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, placeat quia cumque voluptatem blanditiis dolorem vitae error, recusandae, voluptas incidunt ducimus et qui sed aliquam architecto cupiditate provident quidem facere?'
+            />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+
+            <Course
+              image={Bootstrap}
+              title='Bootstrap'
+              description='   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, placeat quia cumque voluptatem blanditiis dolorem vitae error, recusandae, voluptas incidunt ducimus et qui sed aliquam architecto cupiditate provident quidem facere?'
+            />
+
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course
+              image={Ccsharp}
+              title='Ccsharp'
+              description='   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, placeat quia cumque voluptatem blanditiis dolorem vitae error, recusandae, voluptas incidunt ducimus et qui sed aliquam architecto cupiditate provident quidem facere?'
+            />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course
+              image={Komple}
+              title='Komple'
+              description='   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, placeat quia cumque voluptatem blanditiis dolorem vitae error, recusandae, voluptas incidunt ducimus et qui sed aliquam architecto cupiditate provident quidem facere?'
+            />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
--- card/src/Course.js
+++ card/src/Course.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
-function Course() {
+function Course({ image, title, description }) {
     return (
-        <div>Course</div>
+        <div>
+            <Card >
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={image}
+                    title="kurslarım"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {description}
+                    </Typography>
+                </CardContent>
+
+            </Card>
+        </div>
     )
 }
 

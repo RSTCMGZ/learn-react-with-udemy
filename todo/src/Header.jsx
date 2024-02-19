2531448c79d07bfff98ec8ad098cf59351850b72
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-19 23:43:48 +0300

    createtask

---
+++ todo/src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AssingnmentIcon from '@mui/icons-material/Assignment'
+
+function Header() {
+
+    return (
+        <header className='bg-    px-'>
+            <h1>
+                <AssingnmentIcon />
+                To Do List
+            </h1>
+        </header>
+    )
+}
+
+
+export default Header

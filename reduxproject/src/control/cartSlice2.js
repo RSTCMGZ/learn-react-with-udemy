d3d5c870093823ae591850f203924c59c1885b68
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-22 23:48:58 +0300

    cartSlice-add

---
+++ reduxproject/src/control/cartSlice2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function cartSlice2() {
+    return (
+        <div>cartSlice2</div>
+    )
+}
+
+export default cartSlice2
--- reduxproject/src/control/catSlice2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function catSlice2() {
-    return (
-        <div>catSlice2</div>
-    )
-}
-
-export default catSlice2

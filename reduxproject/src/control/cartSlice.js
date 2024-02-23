c9c71cef4a3417d0d5397da0368ff0977f4b71d6
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-24 01:26:06 +0300

    quantity+calculate

--- reduxproject/src/control/cartSlice.js
+++ reduxproject/src/control/cartSlice.js
@@ -26,9 +26,19 @@ const cartSlice = createSlice({
             const cartItem = state.cartItems.find((item) => item.id === action.payload)
             cartItem.quantity -= 1
         },
+        calculateTotal: (state) => {
+            let total = 0
+            let quantity = 0
+            state.cartItems.forEach((item) => {
+                total += item.quantity * item.price
+                quantity += item.quantity
+            })
+            state.quantity = quantity
+            state.total = total
+        },
     },
 });
 
-export const { clearCart, removeItem, increase, decrease } = cartSlice.actions
+export const { clearCart, removeItem, increase, decrease, calculateTotal } = cartSlice.actions
 
 export default cartSlice.reducer;

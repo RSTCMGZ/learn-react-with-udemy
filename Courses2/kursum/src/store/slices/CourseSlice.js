425cdfa8b8a581b8e3b9934e1e847f020fa58459
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-25 16:26:22 +0300

    start-project-with-ReactRedux

---
+++ Courses2/kursum/src/store/slices/CourseSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const courseSlice = createSlice({
+    name: 'form',
+    initialState: {
+        searchTerm: '',
+        data: []
+    },
+    reducers: {
+
+    }
+})
+
+
+export const courseReducer = courseSlice.reducer

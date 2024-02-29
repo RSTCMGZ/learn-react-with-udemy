c24a5257c953afa6bea5024a4a8fb1a748c0e560
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-01 02:25:13 +0300

    add-AlbumList

--- QueryProject/person/src/components/UsersListItem.js
+++ QueryProject/person/src/components/UsersListItem.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import ExpandablePanel from './ExpandablePanel';
+import AlbumList from './AlbumList';
+import { GoTrashcan } from 'react-icons/go'
 
 function UsersListItem({ user }) {
-  return <div >{user.name}</div>;
+  const header = (
+    <>
+      <button style={{ marginRight: '30px', border: 'none' }}>
+        <GoTrashcan />
+      </button>
+      {user.name}
+    </>
+  )
+  return <div >
+    <ExpandablePanel header={header}>
+      <AlbumList user={user} />
+    </ExpandablePanel>
+  </div>;
 }
 
 export default UsersListItem;

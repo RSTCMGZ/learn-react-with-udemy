85b0f0450c5521646cc9b9bc9da2826ea887df40
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-04 00:58:39 +0300

    album-delete

--- QueryProject/person/src/components/AlbumListItem.js
+++ QueryProject/person/src/components/AlbumListItem.js
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import ExpandablePanel from './ExpandablePanel'
+import PhotoList from './PhotoList'
+import { GoTrashcan } from 'react-icons/go'
+import CircularProgress from '@mui/material/CircularProgress';
+import { useRemoveAlbumMutation } from '../store';
 function AlbumListItem({ album }) {
+    const [removeAlbum, results] = useRemoveAlbumMutation()
+    const handleClick = () => {
+        removeAlbum(album)
+    }
+    const header = (
+        <>
+            <button onClick={handleClick} style={{ marginRight: '30px', border: 'none', cursor: 'pointer' }}>
+                {results.isLoading ? (
+                    <CircularProgress style={{ width: '20px', height: '20px' }} />
+                ) :
+                    <GoTrashcan />
+                }
+            </button>
+            {album.title}
+        </>
+    )
     return (
-        <div>{album.title}</div>
+        <div >
+            <ExpandablePanel header={header}>
+                <PhotoList album={album} />
+            </ExpandablePanel>
+        </div>
     )
 }
 

2caeec3947cce595d530d54659b2dc78b8902d0c
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-05 18:54:29 +0300

    photo-Add

--- QueryProject/person/src/components/PhotoList.js
+++ QueryProject/person/src/components/PhotoList.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import { useAddPhotoMutation, useFetchPhotosQuery } from '../store/apis/photosApi'
+import { PhotoListItem } from './PhotoListItem'
+import Button from '@mui/material/Button'
+import CircularProgress from '@mui/material/CircularProgress';
+import Skeleton from '@mui/material/Skeleton';
 
-function PhotoList() {
+
+function PhotoList({ album }) {
+    const { data, isError, isFetching } = useFetchPhotosQuery(album)
+    const [addPhoto, results] = useAddPhotoMutation()
+
+
+    const handlePhotoAdd = () => {
+        addPhoto(album)
+    }
+
+
+    let content;
+    if (isFetching) {
+        content = (
+            <Skeleton variant="rectangular" sx={{ width: '100%', height: '200px' }} />
+        );
+    } else if (isError) {
+        content = <div>Hata Var</div>;
+    } else {
+        content = data.map((photo) => {
+            return <PhotoListItem key={photo.id} photo={photo} />;
+        });
+    }
     return (
-        <div>PhotoList</div>
+        <div>
+            <div className='topArrangement'>
+                <h3>{album.title} Fotoğrafları</h3>
+                <Button variant='outlined' onClick={handlePhotoAdd}>
+                    {results.isLoading ? (
+                        <CircularProgress />
+                    ) : <span>
+                        Foto Ekle+
+                    </span>}
+                </Button>
+            </div>
+            <div>
+                {content}
+            </div>
+        </div>
     )
 }
 
--- QueryProject/person/src/store/index.js
+++ QueryProject/person/src/store/index.js
@@ -2,18 +2,29 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { usersApi } from './apis/usersApi';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { albumsApi } from './apis/albumsApi';
+import { photosApi } from './apis/photosApi';
 
 export const store = configureStore({
   reducer: {
     [usersApi.reducerPath]: usersApi.reducer,
-    [albumsApi.reducerPath]: albumsApi.reducer
+    [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(usersApi.middleware).concat(albumsApi.middleware);
+    return getDefaultMiddleware()
+      .concat(usersApi.middleware)
+      .concat(albumsApi.middleware)
+      .concat(photosApi.middleware)
   },
 });
 
 setupListeners(store.dispatch);
+
+export {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useRemovePhotoMutation,
+} from './apis/photosApi'
 
 export {
   useFetchAlbumsQuery,
